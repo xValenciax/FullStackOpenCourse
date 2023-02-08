4f14a5fbaaff637f7fe7c3d1f2dbed06d899b534
Selim <mamo31st@gmail.com>
2023-02-08 15:05:58 +0200

    added exercise 1.10

---
+++ part1/Exercise1.10/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+const StatisticsLine = ({text, value}) =>{
+  return <p>{text} {value}</p>
+}
+
+const Button = ({text, handler}) => {
+  return <button onClick={handler}>{text}</button>
+}
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = good / all * 100;
+
+
+  return (
+    <div className="App">
+      <h2>give feedback</h2>
+      <Button onClick={() => setGood(good + 1)} text={'good'} />
+      <Button onClick={() => setNeutral(neutral + 1)} text={'neutral'} />
+      <Button onClick={() => setBad(bad + 1)} text={'bad'} />
+
+      <h2>statistics</h2>
+      <StatisticsLine text={'good'} value={good}/>
+      <StatisticsLine text={'neutral'} value={neutral}/>
+      <StatisticsLine text={'bad'} value={bad}/>
+      <StatisticsLine text={'all'} value={all}/>
+      <StatisticsLine text={'average'} value={average}/>
+      <StatisticsLine text={'positive'} value={positive}/>
+    </div>
+  )
+}
+
+export default App
