26cf5f8847129c4289f1cad97ecd8c6f6aaff0a5
Selim <mamo31st@gmail.com>
2023-02-08 13:34:23 +0200

    modified exercise 1.6

--- part1/Exercise1.6/src/App.jsx
+++ part1/Exercise1.6/src/App.jsx
@@ -1,32 +1,16 @@
 import { useState } from 'react';
 
-const Button = ({text, feedbackHandler}) => {
-  return <button style={{padding: '10px', marginRight: '10px'}} onClick={feedbackHandler(text)}>{text}</button>
-}
-
 function App() {
   const [good, setGood] = useState(0);
   const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
 
-  const feedbackHandler = (type) => {
-    switch(type){
-      case 'Good':
-        return () => setGood(good + 1)
-      case 'Neutral':
-        return () => setNeutral(neutral + 1)
-      case 'Bad':
-        return () => setBad(bad + 1)
-    }
-  }
-
   return (
     <div className="App">
       <h2>give feedback</h2>
-
-      <Button text={'Good'} feedbackHandler={feedbackHandler}/>
-      <Button text={'Neutral'} feedbackHandler={feedbackHandler}/>
-      <Button text={'Bad'} feedbackHandler={feedbackHandler}/>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
 
       <h2>statistics</h2>
       <p>Good {good}</p>
