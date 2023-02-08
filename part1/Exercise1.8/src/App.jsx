82cd89bf440ffecdc0d49ab162d73023738438de
Selim <mamo31st@gmail.com>
2023-02-08 13:56:13 +0200

    added exercise 1.8

---
+++ part1/Exercise1.8/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const Statistics = ({feedback}) => {
+  const all = feedback[0] + feedback[1] + feedback[2];
+  const average = all / 3;
+  const positive = feedback[0] / all;
+
+  return <div>
+    <p>good {feedback[0]}</p>
+    <p>neutral {feedback[1]}</p>
+    <p>bad {feedback[2]}</p>
+    <p>all {all}</p>
+    <p>average {average}</p>
+    <p>positive {positive}</p>
+  </div>
+}
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  return (
+    <div className="App">
+      <h2>give feedback</h2>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      <h2>statistics</h2>
+      <Statistics feedback={[good, neutral, bad]}/>
+    </div>
+  )
+}
+
+export default App
