abc9d8bb9db75382b1bcd2a20167bffefff16654
Selim <mamo31st@gmail.com>
2023-07-17 16:43:58 +0300

    Exercise 3.8 Done

--- part3/Exercise3.7_3.8/index.js
+++ part3/Exercise3.7_3.8/index.js
@@ -2,8 +2,29 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+let data = false;
+
+morgan.token('data', (req) => {
+	if (req.body && req.method === 'POST') {
+		return JSON.stringify(req.body);
+	} else return null;
+});
+
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(
+	morgan(function (tokens, req, res) {
+		return [
+			tokens.method(req, res),
+			tokens.url(req, res),
+			tokens.status(req, res),
+			tokens.res(req, res, 'content-length'),
+			'-',
+			tokens['response-time'](req, res),
+			'ms',
+			req.body && req.method === 'POST' ? tokens.data(req) : '',
+		].join(' ');
+	})
+);
 
 let persons = [
 	{
