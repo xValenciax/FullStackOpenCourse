a04dbde4afe29f63e78ea3a3a3e8861f536da6f5
Selim <mamo31st@gmail.com>
2023-08-05 20:27:46 +0300

    task 4.9 done

--- part4/Exercise4.8_4.12/tests/blog_api.test.js
+++ part4/Exercise4.8_4.12/tests/blog_api.test.js
@@ -30,7 +30,7 @@ beforeEach(async () => {
 });
 
 describe('api', () => {
-	test('blogs are returned with correct amount and JSON format', async () => {
+	test('correct amount of blogs returned in JSON  format', async () => {
 		const blogs = await api
 			.get('/api/blogs')
 			.expect(200)
@@ -39,11 +39,32 @@ describe('api', () => {
 		expect(blogs.body).toHaveLength(initialBlogs.length);
 	});
 
-	test('verify the id property', async () => {
+	test('id property is defined', async () => {
 		const blogs = await Blog.find({});
 		const blogToCheck = blogs[0];
 
 		expect(blogToCheck.id).toBeDefined();
+	});
+
+	test('new note is added', async () => {
+		const newBlog = {
+			title: 'First class tests',
+			author: 'Robert C. Martin',
+			url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+			likes: 10,
+		};
+
+		await api
+			.post('/api/blogs')
+			.send(newBlog)
+			.expect(201)
+			.expect('Content-Type', /application\/json/);
+
+		const notesAtEnd = await Blog.find({});
+		expect(notesAtEnd).toHaveLength(initialBlogs.length + 1);
+
+		const titles = notesAtEnd.map((b) => b.title);
+		expect(titles).toContain(newBlog.title);
 	});
 });
 
