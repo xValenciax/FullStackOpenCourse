2731bab3f53cf363c198401d5e46da5a12b0b99b
Selim <mamo31st@gmail.com>
2023-08-05 20:29:13 +0300

    task 4.10 done

--- part4/Exercise4.8_4.12/controllers/blogs.js
+++ part4/Exercise4.8_4.12/controllers/blogs.js
@@ -6,12 +6,11 @@ blogRouter.get('/', async (request, response) => {
 	response.json(blogs);
 });
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
 	const blog = new Blog(request.body);
 
-	blog.save().then((result) => {
-		response.status(201).json(result);
-	});
+	const result = await blog.save();
+	response.status(201).json(result);
 });
 
 module.exports = blogRouter;
